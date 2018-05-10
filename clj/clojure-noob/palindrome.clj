756ade3c7cad079f23164efcbe72807fdd48e83a
Kel Byers <kel_byers@homedepot.com>
2018-05-10 15:59:34 -0500

    palindome in progress

---
+++ clj/clojure-noob/palindrome.clj
@@ -0,0 +1,63 @@
+(str 99)
+(get "abcdef" 2)
+(count "abcdef")
+(/ 3 2)
+(use 'clojure.set)
+(defn round
+  [num]
+  (int (+ 0.5 num)))
+
+(round 1.5)
+
+(defn palindome-number?
+  [num]
+  (let [f-num (str num)
+        r-num (reverse f-num)]
+    (loop [[f-dig & f-num] f-num
+           [r-dig & r-num] r-num]
+      (if (nil? f-dig)
+        true
+        (if (not= f-dig r-dig)
+          false
+          (recur f-num r-num))))))
+
+(defn pal-prod
+  [a b]
+  (let [p (* a b)]
+    (if (palindome-number? p)
+      #{p}
+      #{})))
+
+(palindome-number? 99)
+(palindome-number? 9019)
+(pal-prod 91 99)
+
+(defn pal-prod-rng-b
+  [a min-b max-b]
+  (loop [b min-b
+         pals #{}]
+    (if (> b max-b)
+      pals
+      (recur (inc b) (union pals (pal-prod a b))))))
+
+(pal-prod-rng-b 91 99 99)
+
+(pal-prod-rng-b 11 10 99)
+
+(defn pal-prod-rng
+  [min-a max-a min-b max-b]
+  (loop [a min-a
+         pals #{}]
+    (if (> a max-a)
+      (set (sort pals))
+      (recur (inc a) (into pals (pal-prod-rng-b a min-b max-b))))))
+
+(pal-prod-rng 11 11 10 99)
+
+(= (pal-prod-rng-b 11 10 99) (pal-prod-rng 11 11 10 99))
+
+(pal-prod-rng 10 99 10 99)
+
+(apply sorted-set (pal-prod-rng 10 99 10 99))
+
+(apply max (pal-prod-rng 11 11 10 99))
